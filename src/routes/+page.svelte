06bef9c4b7653bb5aad08aa11bc8ee1352a63e8d
Nathan Hay <72686983+NathanBHay@users.noreply.github.com>
2024-08-03 15:15:08 +1000

    Added Tauri & Svelte template

---
+++ src/routes/+page.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+  // Scripts go here
+</script>
+
+<div class="container">
+  <h1>Welcome to App!</h1>
+  <p>This is an example application.</p>
+</div>
+
+<style>
+  :root {
+    font-family: Inter, Avenir, Helvetica, Arial, sans-serif;
+    font-size: 16px;
+    line-height: 24px;
+    font-weight: 400;
+
+    color: #0f0f0f;
+    background-color: #f6f6f6;
+
+    font-synthesis: none;
+    text-rendering: optimizeLegibility;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    -webkit-text-size-adjust: 100%;
+  }
+</style>
